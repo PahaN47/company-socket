ad9b8def6565319544296b178f1fde8db4f04b29
PahaN47 <pahan4781@gmail.com>
2024-02-12 21:59:10 +0300

    срочные изменения перед сдачей, которые не закоммитил

--- src/company_socket/settings.py
+++ src/company_socket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3)h4cllkiq)hyk3mv3in87ri^#pnl*svgr^%a2q)6s^2df!ai2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
